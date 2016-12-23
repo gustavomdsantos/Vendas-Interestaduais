ed8e88d4dc8afb72e620d12eec10fa555840971c
Gustavo Moraes <gustavosotnas1@gmail.com>
2016-12-23 17:44:17 -0200

    Implementa codigo que lê os dados em YAML e converte em R
    Com código de instalar pacotes de: http://stackoverflow.com/a/9341833

--- src/vendas-interestaduais.R
+++ src/vendas-interestaduais.R
@@ -1,1 +1,18 @@
-sqrt(9)
+# Vendas Interestaduais
+# ---------------------
+# Autor: Gustavo Moraes
+
+# Função que verifica se um pacote/biblioteca para R foi instalado. Se não, instala.
+instala_dependencia <- function(x)
+{
+  if (!require(x,character.only = TRUE))
+  {
+    install.packages(x,dep=TRUE)
+    if(!require(x,character.only = TRUE)) stop("Package not found")
+  }
+}
+
+instala_dependencia("yaml")
+library(yaml)
+
+dados <- yaml.load_file("src/carga-tributaria-brasil.yml")
